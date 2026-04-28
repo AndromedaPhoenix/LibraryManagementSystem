0e5217392cc8ea6252352d6a807ab993d77bfaae
Padam <Padamdmgr8@gmail.com>
2026-04-29 03:54:02 +1000

    Initial setup of maven projet and creating associated  folder named src with main classes and test class folder.

---
+++ src/main/java/com/naps/Library/App.java
@@ -0,0 +1,13 @@
+package com.naps.Library;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
