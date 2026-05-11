fe4c926d23e88e83b601927b9b8cbd35ab691395
sugal <sugaladh2@gmail.com>
2026-05-12 09:32:29 +1000

    To add the book class

---
+++ src/main/java/com/naps/Library/Book.java
@@ -0,0 +1,18 @@
+package com.naps.Library;
+
+public class Book {    String isbn, title, author;
+    boolean borrowed;
+    LocalDate dueDate;
+
+    public Book(String isbn, String title, String author) {
+        this.isbn = isbn;
+        this.title = title;
+        this.author = author;
+    }
+
+    @Override
+    public String toString() {
+        return isbn + " | " + title + " | " + author +
+                (borrowed ? " | Borrowed (due " + dueDate + ")" : " | Available");
+    }
+}
